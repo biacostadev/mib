74c7c78ff6da83396e986993d1722969ebb029ef
Bia Costa <anabeatrizcosta.o@gmail.com>
2021-07-25 18:04:50 -0300

    novos ajustes

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
-window.onload = initPage;
+
 
 function initPage() {
+  console.log("iniciou")
   const numeroTorres = document.getElementById('numeroTorres');
   const numeroApartamentos = document.getElementById('numeroApartamentos');
   const min = 1;
@@ -23,18 +24,51 @@ function initPage() {
   }
 }
 
+window.onload = initPage();
 
-const nome = document.getElementById('nome')
-const telefone = document.getElementById('telefone')
-const email = document.getElementById('email')
-const mensagem = document.getElementById('mensagem')
+/* Máscaras ER */
+function mascara(o,f){
+  v_obj=o
+  v_fun=f
+  setTimeout("execmascara()",1)
+}
+function execmascara(){
+  v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+  v=v.replace(/\D/g,"");             //Remove tudo o que não é dígito
+  v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+  v=v.replace(/(\d)(\d{4})$/,"$1-$2");    //Coloca hífen entre o quarto e o quinto dígitos
+  return v;
+}
+function id( el ){
+return document.getElementById( el );
+}
+window.onload = function(){
+id('telefone').onkeypress = function(){
+  mascara( this, mtel );
+}
+}
+
 
 
 
 const buttonCodificar = document.getElementById("anexar");
+const nome = document.getElementById("nome");
+const telefone = document.getElementById("telefone");
+const email = document.getElementById("email");
+const mensagem = document.getElementById("mensagem");
 buttonCodificar.addEventListener("click", anexar);
+
 function anexar() {
+  if(nome.value == '' || telefone.value == '' || email.value == '' || mensagem.value == ''){
+    alert('Por favor, preencha todos os campos do formulário.');      
+        return false;
+  } else {
     console.log('foi')
-    console.log(nome.value)
-    window.open('mailto:vendas@mib.ind.br')
+  window.open('mailto:vendas@mib.ind.br?subject=Contato site MIB&body=Nome: ' + nome.value + '%0DTelefone: ' + telefone.value + '%0DEmail: ' + email.value +  '%0DMensagem: '  + mensagem.value)
+  }
+
+
+  
 }
